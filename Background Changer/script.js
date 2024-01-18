eea78da37154d70892b5c7abf2b564728e30adcc
Pranav Gupta <pranavgupta1801@gmail.com>
2024-01-18 11:01:43 +0530

    Added Randomly BG Color Generating Function in Backhground Changer Mini Project

--- Background Changer/script.js
+++ Background Changer/script.js
@@ -1,5 +1,6 @@
 const selectedColorCode = document.getElementById("selected-color");
 
+// Changing Background Color Using Color Picker
 function changeClick(){
     const colorPicker = document.getElementById("color-picker").value;
     // changeColor('Yellow');
@@ -7,6 +8,18 @@ function changeClick(){
     document.body.style.background = colorPicker;
 }
 
+
+// Randomly Changing Background Color
+const generateRandomColor = () => {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    changeColor(color);
+}
+
 function changeColor(color){
     document.body.style.background = color;
+    selectedColorCode.innerHTML = color;
 }
